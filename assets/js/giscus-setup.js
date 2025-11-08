89ea50cdff942f94decee4494a23e80e51c07221
auliakd <135806899+auliakd@users.noreply.github.com>
2025-11-08 09:06:24 +0000

    Deploying to gh-pages from @ auliakd/auliakd.github.io@f83d4ae59051a6c7a21d8354516b1d84e5f5dbdd 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "auliakd/auliakartikadewi.github.io",
+    "data-repo": "auliakd/auliakd.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
