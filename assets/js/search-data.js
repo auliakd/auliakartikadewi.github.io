28d73a4d866b2cb081f37a86138981b8cbc428c5
auliakd <135806899+auliakd@users.noreply.github.com>
2025-11-09 06:32:05 +0000

    Deploying to gh-pages from @ auliakd/auliakd.github.io@91f68343bbb0a3bf2061f73d7639f93dfe135514 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -105,6 +105,28 @@ ninja.data = [{
             window.location.href = "/blog/2024/typograms/";
           
         },
+      },{id: "post-a-post-that-can-be-cited",
+        
+          title: "a post that can be cited",
+        
+        description: "this is what a post that can be cited looks like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/post-citation/";
+          
+        },
+      },{id: "post-a-post-with-pseudo-code",
+        
+          title: "a post with pseudo code",
+        
+        description: "this is what included pseudo code could look like",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2024/pseudocode/";
+          
+        },
       },{id: "post-my-first-days-in-singapore",
         
           title: "My First Days in Singapore 🇸🇬",
